8384d91f75c98ace9e1ae3115d78d8e314b4c0d1
henry5720 <54354262+henry5720@users.noreply.github.com>
2022-02-18 21:25:45 +0800

    Add files via upload

--- week-7/practice/flask/convert_josn.py
+++ week-7/practice/flask/convert_josn.py
@@ -4,13 +4,25 @@ app = Flask(__name__)
 
 @app.route('/returnjson', methods = ['GET'])
 def ReturnJSON():
-	if(request.method == 'GET'):
-		data = {
-			"Modules" : 15,
-			"Subject" : "Data Structures and Algorithms",
-		}
-
-		return jsonify(data)
-
+    # [判斷] (HTTP請求方法) 如果是GET
+    if (request.method == "GET"):
+        # [獲取] Url 參數 (query string)
+        username = request.args.get("username", "")
+        # print(username)
+        # [判斷] 如果 參數 = "ply"
+        if(username=="ply"):
+            # [創建] dict 
+            data={
+                    "data":{
+                        "id":3,
+                        "name":"強強",
+                        "username":"strong"
+                    }
+                }
+            # [回傳] 返回包含json格式資料響應(body)的方法
+            return jsonify(data)
+        else:
+            return dict(data=None)
+    
 if __name__=='__main__':
 	app.run(debug=True)
