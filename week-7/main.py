b18996717a2d1537e4b3d9325254ddfd10c7945c
henry5720 <54354262+henry5720@users.noreply.github.com>
2022-03-06 00:35:10 +0800

    Update main.py

--- week-7/main.py
+++ week-7/main.py
@@ -3,4 +3,4 @@ from webapp import create_app
 if __name__ == '__main__':
     app = create_app('development')
     app.config['JSON_AS_ASCII'] = False
-    app.run(use_reloader=True, port=3000, host='0.0.0.0')
+    app.run(use_reloader=True, port=5000, host='0.0.0.0')
